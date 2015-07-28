785a40a9f6b5782e8f303b17b88328541f475afd
euwbah <euwbah@gmail.com>
2015-07-28 20:06:33 +0800

    asdf

--- New Project/NativeValue.java
+++ New Project/NativeValue.java
@@ -1,2 +1,6 @@
 import java.util.*;
 import java.io.*;
+
+public class NativeValue {
+
+}
--- New Project/Parser.java
+++ New Project/Parser.java
@@ -1,2 +1,25 @@
 import java.util.*;
 import java.io.*;
+import java.util.regex.Pattern;
+
+public class Parser
+{
+    public static class P_Literal
+    {
+        public static Pattern numberPattern = Pattern.compile("^\\s*[0-9.]\\s*$");
+        public static Pattern boolPattern = Pattern.compile("^\\s*true\\s*$|^\\s*false\\s*$");
+        public static Pattern stringPattern = Pattern.compile("^\\s*\".*\"\\s*$|^\\s*\'.*\'\\s*$");
+
+        public static boolean isNumber(String literalCase) {
+            return numberPattern.matcher(literalCase).matches();
+        }
+
+        public static boolean isBool(String literalCase) {
+            return boolPattern.matcher(literalCase).matches();
+        }
+
+        public static boolean isString(String literalCase) {
+            return stringPattern.matcher(literalCase).mastches();
+        }
+    }
+}
